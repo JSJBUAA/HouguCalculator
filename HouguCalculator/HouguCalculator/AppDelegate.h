9aa1123854393b64fbcf1926939afa60dd76aaf3
JSJBUAA <jsjbuaa@163.com>
2017-12-08 19:18:29 +0800

    bb

---
+++ HouguCalculator/HouguCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HouguCalculator
+//
+//  Created by 姜世举 on 2017/12/8.
+//  Copyright © 2017年 4Paradigm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HouguCalculator/HouguCalculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HouguCalculator
+//
+//  Created by 姜世举 on 2017/12/8.
+//  Copyright © 2017年 4Paradigm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
